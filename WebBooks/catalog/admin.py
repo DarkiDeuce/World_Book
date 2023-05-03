7ae4c31cdb495fe2f0f470b3b7733823ae2229f1
DarkiDeuce <darki.googl@,ail.ru>
2023-05-03 17:49:21 +0700

    Add file and update BD

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Author, Book, Genre, Language, Status, BookInstance
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
+admin.site.register(BookInstance)
